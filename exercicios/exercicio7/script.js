8e84ef4eb8bf6a8439d0245c563fa1581931d5a8
VilarimLucas <98033137+VilarimLucas@users.noreply.github.com>
2023-09-08 13:09:06 -0300

    exercício 8

--- exercicios/exercicio7/script.js
+++ exercicios/exercicio7/script.js
@@ -1,17 +1,27 @@
-window.onload = function () {
-    document.getElementById("btn").addEventListener('click', btn_click);
-}
+var valores = [];
 
-function btn_click() {
-    var temp = parseFloat(document.getElementById('temp').value);
-    var grau = document.getElementById('grau').value
-    if (grau == 'celcius') {
-        // Convertendo CELSIUS EM FARENHEIT
-        temp = (temp*1.8)+32;
-        document.getElementById('resultado').innerHTML = `<br><b>TEMPERATURA: </b>${temp}<b>ºF</b>`;
-    } else {
-        // Convertendo FARENHEIT EM CELSIUS
-        temp = (temp-32)/1.8;
-        document.getElementById('resultado').innerHTML = `<br><b>TEMPERATURA: </b>${temp}<b>ºC</b>`;
-    }
-}
+        function adicionarValor() {
+            var valor = parseInt(document.getElementById('valor').value);
+            if (!isNaN(valor)) {
+                valores.push(valor);
+                document.getElementById('valor').value = ''; // Limpa o campo de entrada
+            }
+        }
+
+        function mostrarValores() {
+            document.getElementById('resultado').innerHTML = `<hr><b>Valores Inseridos:</b> ${valores.join(', ')}`;
+        }
+
+        function mostrarMaiorValor() {
+            if (valores.length > 0) {
+                var maiorValor = Math.max.apply(null, valores);
+                document.getElementById('resultado').innerHTML = `<hr><b>Maior Valor: </b> ${maiorValor}`;
+            }
+        }
+
+        function mostrarMenorValor() {
+            if (valores.length > 0) {
+                var menorValor = Math.min.apply(null, valores);
+                document.getElementById('resultado').innerHTML = `<hr><b>Menor Valor: </b> ${menorValor}`;
+            }
+        }
